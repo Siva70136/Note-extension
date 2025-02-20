f523cef6fbd08a663ea96f3cc234264342d7c03b
Siva Mallikharjuna Cheerla <siva.ch@talview.com>
2025-02-20 16:34:09 +0530

    modified workflow

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//     entry: "./src/index.js",
-//     output: {
-//       filename: "bundle.js",
-//       path: __dirname + "/dist"
-//     },
-//     mode: "production"
-//   };
-  
