51e34d015a3c5bd2fee038cf808b32d1a475adf8
Siva Mallikharjuna Cheerla <siva.ch@talview.com>
2025-02-20 15:37:32 +0530

    eslint added

--- extension.config.js
+++ extension.config.js
@@ -1,13 +1,16 @@
-module.exports = {
-    browser: {
-        chrome: {
-            profile: "path/to/custom/profile",
-        },
-        firefox: {
-            profile: "path/to/custom/firefox-profile",
-        },
-        edge: {
-            profile: "edge-profile",
-        },
-    },
-};
+// module.exports = {
+//     browser: {
+//         chrome: {
+//           profile: 'path/to/chrome',
+//           preferences: { darkMode: true },
+//           browserFlags: ['--disable-web-security'],
+//           chromeBinary: 'path/to/custom/chrome',
+//         },
+//         edge:{
+//             profile: 'path/to/edge',
+//           },
+//         firefox: {
+//           geckoBinary: 'path/to/custom/firefox',
+//         }
+//       }
+// };
--- service-worker.js
+++ service-worker.js
@@ -27,25 +27,14 @@
 // //);
 
 // // When the user has accepted what is typed into the omnibox.
-// chrome.action.onClicked.addListener((tab) => {
+function change() {
+    document.body.style.backgroundColor = 'red';
 
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     func: () => {
-//       alert("Script");
-//       // let isDark = document.documentElement.style.getPropertyValue('--background-color') === 'black';
-//       // console.log(isDark)
+}
+chrome.action.onClicked.addListener((tab) => {
 
-//       // if (isDark) {
-//       //   document.documentElement.style.setProperty('--background-color', 'white');
-//       //   document.documentElement.style.setProperty('--text-color', 'black');
-//       // } else {
-//       //   document.documentElement.style.setProperty('--background-color', 'black');
-//       //   document.documentElement.style.setProperty('--text-color', 'white');
-//       // }
-
-//       // document.body.style.backgroundColor = document.documentElement.style.getPropertyValue('--background-color');
-//       // document.body.style.color = document.documentElement.style.getPropertyValue('--text-color');
-//     }
-//   })
-// });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: change
+    })
+});
